b951a00c5403251ca1409eae1c74e98555b6af67
Joe Gallotta <joeygallotta@gmail.com>
2018-10-26 14:31:53 -0500

    create two users in db

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
-  post '/users' => 'users#create'
+  namespace :api do
+    post '/users' => 'users#create'
+  end
 end
